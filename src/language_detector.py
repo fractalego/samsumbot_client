61595cf824665b0d6eeafee38c302f57859c2c16
Fractalego <albertocetoli@gmail.com>
2022-05-11 13:18:24 +0100

    Updated knowledge and language detector

--- src/language_detector.py
+++ src/language_detector.py
@@ -6,8 +6,15 @@ class LanguageDetector:
         self._model = cld2
 
     def is_english(self, utterance):
+        if len(utterance.split()) <= 1:
+            return True
+
         _, _, details = cld2.detect(utterance)
-        if details[0][0].lower() in ["english", "unknown"]:
+
+        if details[0][0].lower() == "unknown":
+            return True
+
+        if any(item[0].lower() == "english" for item in details):
             return True
 
         return False
